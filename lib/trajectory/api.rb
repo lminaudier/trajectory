3b5d80bf498b2d35917a9665cec75566f6dc451c
Loïc Minaudier <loic.minaudier@gmail.com>
2013-02-22 11:21:19 +0100

    Add missing module namespace

--- lib/trajectory/api.rb
+++ lib/trajectory/api.rb
@@ -1,35 +1,37 @@
 require 'httparty'
 
-class Api
-  include HTTParty
-  base_uri "http://www.apptrajectory.com/api/#{ENV['TRAJECTORY_API_KEY']}/accounts/#{ENV['TRAJECTORY_ACCOUNT_KEYWORD']}"
+module Trajectory
+  class Api
+    include HTTParty
+    base_uri "http://www.apptrajectory.com/api/#{ENV['TRAJECTORY_API_KEY']}/accounts/#{ENV['TRAJECTORY_ACCOUNT_KEYWORD']}"
 
-  def self.projects
-    new.projects
-  end
+    def self.projects
+      new.projects
+    end
 
-  def projects
-    JSON.parse get_json("/projects.json")
-  end
+    def projects
+      JSON.parse get_json("/projects.json")
+    end
 
-  def self.stories_for_project(keyword)
-    new.stories_for_project(keyword)
-  end
+    def self.stories_for_project(keyword)
+      new.stories_for_project(keyword)
+    end
 
-  def stories_for_project(project)
-    JSON.parse(get_json("/projects/#{project.keyword}/stories.json"))['stories']
-  end
+    def stories_for_project(project)
+      JSON.parse(get_json("/projects/#{project.keyword}/stories.json"))['stories']
+    end
 
-  private
-  def get_json(url)
-    get_body(url, options)
-  end
+    private
+    def get_json(url)
+      get_body(url, options)
+    end
 
-  def options
-    {:headers => {'Content-Type' => 'application/json'}}
-  end
+    def options
+      {:headers => {'Content-Type' => 'application/json'}}
+    end
 
-  def get_body(*args)
-    self.class.get(*args).body
+    def get_body(*args)
+      self.class.get(*args).body
+    end
   end
 end
