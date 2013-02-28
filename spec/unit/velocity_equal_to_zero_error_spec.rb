145e76d23eabf0dc6427f58b79a5bb5a3f7a46e4
Loïc Minaudier <loic.minaudier@gmail.com>
2013-02-28 13:13:09 +0100

    Remove useless Factory

--- spec/unit/velocity_equal_to_zero_error_spec.rb
+++ spec/unit/velocity_equal_to_zero_error_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 module Trajectory
   describe VelocityEqualToZeroError do
     it 'can displays that velocity of a project is equal to zero' do
-      project = Fabricate(:project)
+      project = double
       VelocityEqualToZeroError.new(project).to_s.should == "Estimated velocity of #{project.inspect} is equal to 0."
     end
   end
