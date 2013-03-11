3630fef930952964db16f86852a8706e9a4b9e1d
Loïc Minaudier <loic.minaudier@gmail.com>
2013-03-11 10:57:08 +0100

    Configure SimpleCov

--- spec/support/simple_cov.rb
+++ spec/support/simple_cov.rb
@@ -1,2 +1,8 @@
 require 'simplecov'
-SimpleCov.start
+SimpleCov.start do
+  add_filter '/spec/'
+  add_group 'Domain', 'lib/trajectory/domain'
+  add_group 'Data Access', 'lib/trajectory/data_access'
+  add_group 'Exceptions', 'lib/trajectory/exceptions'
+  add_group 'Core Ext', 'lib/trajectory/core_ext'
+end
